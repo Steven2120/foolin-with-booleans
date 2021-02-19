5b7c75aefd71422004dd6ad4ea654da105da4e58
Steven Moyano <stevenmoyano1@gmail.com>
2021-02-19 11:55:14 -0600

    passed all tests

--- main.js
+++ main.js
@@ -1,7 +1,61 @@
 /********************
  * YOUR CODE BELOW! *
  ********************/
+function moreThan5(num) {
+  return num > 5;
+}
 
+function isNewTopScore(scr, topScr) {
+  return scr > topScr;
+}
+
+function isInDanger(grd) {
+  return grd >= 60 && grd <= 71;
+}
+
+function isCoasting(grd1) {
+  return grd1 >= 72 && grd1 <= 83;
+}
+
+function isSucceeding(grd2) {
+  return grd2 >= 84 && grd2 <= 92;
+}
+
+function isFailing(grd3) {
+  return grd3 < 60;
+}
+
+function isAcing(grd4) {
+  return grd4 > 92;
+}
+
+function isStudent(str) {
+  return str ==="student";
+}
+
+function isTeacher(str1) {
+  return str1 === "teacher";
+}
+
+function isAdmin(str2) {
+  return str2 === "admin";
+}
+
+function isElementary(str3) {
+  return str3 === "elementary";
+}
+
+function areDifferentPeople(str4, str5) {
+  return str4 !== str5;
+}
+
+function isMiddleSchoolTeacher(str6, lvl) {
+  return str6 === "teacher" && lvl >= 6 && lvl <= 8;
+}
+
+function notAnElementarySchoolAdministrator(str7, str8) {
+  return str7 !== "elementary" || str8 !== "admin";
+} 
 
 
 
